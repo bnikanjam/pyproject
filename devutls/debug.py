c598d4060802fb7b79367abb2f30854f7eadcb12
Babak Nikanjam <babak.nikanjam@gmail.com>
2020-01-13 23:55:22 -0800

    Add benchmark decorator for timing functions

--- devutls/debug.py
+++ devutls/debug.py
@@ -1,5 +1,6 @@
 import colorama
 from functools import wraps
+from timeit import default_timer as timer
 
 
 def print_blue(text):
@@ -21,7 +22,30 @@ def print_green(text):
 def trace(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        output = func(*args, **kwargs)
-        print_yellow(f'{func.__name__}({args}, {kwargs}) -> {output}')
-        return output
+        return_of_decorated_func = func(*args, **kwargs)
+        print_yellow(f'{func.__name__}({args}, {kwargs}) -> {return_of_decorated_func}')
+        return return_of_decorated_func
+    return wrapper
+
+
+def benchmark(func):
+    """Print runtime of the decorated function"""
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = timer()
+        return_of_decorated_func = func(*args, **kwargs)
+        run_time = timer() - start_time
+
+        if run_time >= 1:
+            print(f"\nRUNTIME of {func.__name__}: {round(run_time, 2)} seconds.")
+        elif run_time >= 10 ** -3:
+            print(f"\nRUNTIME of {func.__name__}: {round(run_time*10**3, 2)} milliseconds.")
+        elif run_time >= 10 ** -6:
+            print(f"\nRUNTIME of {func.__name__}: {round(run_time*10**6, 2)} microseconds.")
+        elif run_time >= 10 ** -9:
+            print(f"\nRUNTIME of {func.__name__}: {round(run_time*10**9, 2)} nanoseconds.")
+
+        return return_of_decorated_func
+
     return wrapper
